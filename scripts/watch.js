b9f99d1fd37182d38871877be7743fce871aacd9
Colin Kierans <kierans@monstercat.com>
2016-07-22 11:09:44 -0700

    Fixed watch script. Based off old one.

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,6 +1,25 @@
 var spy = require("eye-spy");
 var spawn = require('child_process').spawn;
 
+var server;
+
+function start(done) {
+  if (server) {
+    console.log("Restarting server.");
+    server.kill();
+  }
+  else {
+    console.log("Starting server.");
+  }
+
+  server = spawn("node", ["server/index"], { 
+    stdio: "inherit", 
+    env: process.env 
+  });
+
+  if (done) setTimeout(done, 2000);
+}
+
 function build (done) {
   console.log("Building client.")
   builder = spawn("npm", ["run", "build"], { stdio: "inherit" });
@@ -9,6 +28,12 @@ function build (done) {
   });
 }
 
-spy(".", /^(src|components)\/.*\.(json|coffee|js|jade|less|css)$/, function (path, done) {
+spy(".", /^src\/((?!client).)*\.coffee$/, function (path, done){
+  start(done);
+});
+
+spy(".", /^(src\/components|components)\/.*\.(json|coffee|js|jade|less|css)$/, function (path, done) {
   build(done);
 });
+
+start();
